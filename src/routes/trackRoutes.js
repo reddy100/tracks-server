c9194c891d26e8eae4727e5432aaa9873988b1e3
Abishek Reddy <abishekwdaru@gmail.com>
2020-05-01 12:56:31 +0530

    first commit

---
+++ src/routes/trackRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const requireAuth = require('../middlewares/requiredAuth')
+
+const Track = mongoose.model('Track');
+
+const router = express.Router();
+router.use(requireAuth);
+
+router.get('/tracks', async (req, res) => {
+    const tracks = await Track.find({userId: req.user._id});
+
+    res.send(tracks);
+});
+
+router.post('/tracks', async(req, res) => {
+    const {name, locations} = req.body;
+
+    if(!name || !locations) {
+        return res
+        .status(422)
+        .end({error: 'You must provide a name and locations'})
+    }
+
+    const track = new Track({name, locations, userId: req.user._id});
+    await track.save();
+    res.send(track);
+})
+
+module.exports = router;
